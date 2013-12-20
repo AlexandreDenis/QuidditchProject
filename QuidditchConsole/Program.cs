396333787dff6854b92bc83f56bd31fd6bc5c0d8
Adrien <adrien.calime@gmail.com>
2013-12-20 15:58:14 +0100

    creation pl

--- QuidditchConsole/Program.cs
+++ QuidditchConsole/Program.cs
@@ -4,12 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using BusinessLayer;
+
 namespace QuidditchConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
+            CoupeManager cp = new CoupeManager();
+
+
+            /*
+            int choix = -1;
+            while (choix != 3)
+            {
+                Console.WriteLine("Que voulez-vous faire?");
+                Console.WriteLine("   0 -> afficher la liste des matchs");
+                Console.WriteLine("   1 -> afficher la liste des stades");
+                Console.WriteLine("   2 -> afficher la liste des attrapeurs");
+                Console.WriteLine("   3 -> quitter");
+
+                try
+                {
+                    choix = int.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    continue;
+                }
+                switch (choix)
+                {
+
+                }
+            }*/
         }
     }
 }
