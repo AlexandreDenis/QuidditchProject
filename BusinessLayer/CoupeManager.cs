db6591a64081e9fb658088532b20594c220a55e9
AlexandreDenis <alexandre.denis@dbmail.com>
2013-12-20 15:27:37 +0100

    Creation de la classe CoupeManager

---
+++ BusinessLayer/CoupeManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    class CoupeManager
+    {
+        DalManager _manager;
+
+        public CoupeManager()
+        {
+            _manager = new DalManager();
+        }
+
+
+    }
+}
