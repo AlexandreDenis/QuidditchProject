2d771466278452e53c39d75617e585db29b84353
Adrien <adrien.calime@gmail.com>
2013-12-20 15:31:21 +0100

    creation dal manager

--- StubDataAccessLayer/DalManager.cs
+++ StubDataAccessLayer/DalManager.cs
@@ -7,13 +7,39 @@ using EntitiesLayer;
 
 namespace StubDataAccessLayer
 {
-    class DalManager
+    public class DalManager
     {
         public List<Joueur> GetAllJoueurs()
         {
             List<Joueur> joueurs = new List<Joueur>();
 
+            joueurs.Add(1, "nom1", "prenom1", DateTime.Now, PosteJoueur.Poursuiveur, 2, 3);
+
             return joueurs;
+        }
+        public List<Equipe> GetAllEquipe()
+        {
+            List<Equipe> equipes = new List<Equipe>();
+
+            return equipes;
+        }
+        public List<Stade> GetAllStades()
+        {
+            List<Stade> stades = new List<Stade>();
+
+            return stades;
+        }
+        public List<Match> GetAllMatchs()
+        {
+            List<Match> matchs = new List<Match>();
+
+            return matchs;
+        }
+        public List<Coupe> GetAllCoupes()
+        {
+            List<Coupe> coupes = new List<Coupe>();
+
+            return coupes;
         }
     }
 }
